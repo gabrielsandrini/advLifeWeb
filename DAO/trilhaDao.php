bafcf97b2401100468c9cf5d7c98dbeb396357db
Gabriel Albinati Sandrini <gabriel.sandrini@outlook.com>
2018-08-09 23:36:05 -0300

    TrilhaDao com pesquisa finalizada

---
+++ DAO/trilhaDao.php
@@ -0,0 +1,40 @@
+<?php
+include_once 'classeCrudGenerico.php';
+class trilhaDao
+{
+
+    private function makeSqlForSearch($array)
+    {
+        $apelido = isset($array['apelido']) ? $array['apelido'] : null;
+        $distMax = isset($array['distMax']) ? $array['distMax'] : null;
+        $distMin = isset($array['distMin']) ? $array['distMin'] : null;
+        $nickname = isset($array['nickname']) ? $array['nickname'] : null;
+        $sql = "select * from tbtrilha where 1 = 1";
+        
+        if ($apelido)
+        {
+            $sql .= " and apelido like '%$apelido%'";
+        }
+        if ($distMax)
+        {
+            $sql .= " and distancia <= '$distMax'";
+        }
+        if ($distMin)
+        {
+            $sql .= " and distancia >= '$distMin'";
+        }
+        if ($nickname)
+        {
+            $sql .= " and nicknameUsuario like '%$nickname%'";
+        }
+        
+        return $sql;
+    }
+    public function searchTracks($array)
+    {
+        $sql = $this->makeSqlForSearch($array);
+        $crudGenerico = new CrudGenerico();
+        $resultado = mysqli_fetch_all($crudGenerico->fQuery($sql));
+        return $resultado;
+    }
+}
