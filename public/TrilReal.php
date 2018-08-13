8526e1b265f50d04044ba402ae8c86f16915338d
Gabriel Albinati Sandrini <gabriel.sandrini@outlook.com>
2018-08-13 09:38:49 -0300

    Card finished

--- public/TrilReal.php
+++ public/TrilReal.php
@@ -1,26 +1,40 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title>Trilhas Utilizadas</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet" type="text/css" />
-</head>
+    <head>
+        <title>Trilhas Utilizadas</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" rel="stylesheet" type="text/css" />
+        <link href="card.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+    </head>
 
-<body>
-    <div class="container">
-        <header class="pagina"> Trilhas Utilizadas</header>
-        <a href="SelecTril.php">
+    <body>
+        <div class="container">
+            <h1 class="tituloTelaDeBusca">Trilhas utilizadas</h1>
+            <div class="card">
+                <div class="cardTitle"></div>
+                <div> 
+                    <img class="cardImage" src="../Imagens e coisas de mídia/Trilha.jpeg" alt="imagem da trilha">
+                </div>
+                <div> 
+                    <h1 class="cardTitle">Nome da trilha</h1>
+                    <h3 class="cardAtribute">Distância: 50km</h3>
+                    <h3 class="cardAtribute">Nickname: Friboiusdjkj</h3>
+                    <h3 class="cardAtribute">Data de gravação: </h3>
+                </div>
+            </div>
+            <a href="Menu.php" class="botaorandom">Retornar</a>
+        </div>
+    </body>
+
+</html> 
+<!--<a href="SelecTril.php">
             <div class="detail">
                 <div class="stat1"> Mapa da Trilha e nome </div>
                 <div class="stat2"> Distância:<br> X Km </div>
                 <div class="stat2"> Tempo:<br> Xh Xmin</div>
                 <div class="stat2"> Data da realização: DD/MM/AAAA</div>
             </div>
-        </a>
-        <a href="Menu.php" class="botaorandom">Retornar</a>
-    </div>
-</body>
-
-</html>
+        </a>-->
