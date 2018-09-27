b04e89f009f4395c03f65e1f264a5e1d26ea567e
Gabriel Albinati Sandrini <gabriel.sandrini@outlook.com>
2018-09-26 21:08:18 -0300

    Alteração da pagina de trilha com consulta no banco

--- public/SelecTril.php
+++ public/SelecTril.php
@@ -16,16 +16,25 @@ $title = isset($trilha['apelido']) ? $trilha['apelido'] : "Adv Life";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet" type="text/css" />
+    
+    <style>
+        ul{
+          list-style-type: none;  
+        }
+    </style>
 </head>
 
 <body>
     <div class="container2">
         <header class="pagina"> <?php echo $trilha['apelido'];?> </header>
         <img src="../Imagens e coisas de mídia/Trilha.jpeg" alt="Trilha" style="width: 70%; height: 70%; margin-left: 120px">
-        <br> Distância: Xkm<br> 
-        Tempo de duração: Xh Xmin<br> 
-        Usuário responsável: nickname<br> 
-        Data de gravação: DD/MM/AAAA<br>
+        <br>
+        <ul>
+            <li> Distância: <?php echo $trilha['distancia']; ?> Km </li> 
+            <li> Obstaculos: <?php echo $trilha['obstaculos']; ?> </li>
+            <li> Usuário responsável: <?php echo $trilha['nicknameUsuario']; ?> </li> 
+            <li> Data de gravação: <?php echo $trilha['dataGravacao']; ?> </li>
+        </ul>
         <a href="Menu.php" class="botaorandom">Retornar</a>
         <a href="avaliar.php" class="botaorandom">Avaliar</a>
         <a href="mostraAvaliacao.php" class="botaorandom">Avaliações</a>
