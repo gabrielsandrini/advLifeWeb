4b99039b597f7d3735c4cf96fbc9b455d2192074
Gabriel Albinati Sandrini <gabriel.sandrini@outlook.com>
2018-12-04 16:23:59 -0200

    Refatoração do código em RegTril.php

--- public/RegTril.php
+++ public/RegTril.php
@@ -14,73 +14,12 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="style.css" rel="stylesheet" type="text/css" />
+        <!--JQuery link:-->
         <script type="text/javascript" src="JQuery.js"></script>
+        
+        <!--Functions about GPS:-->
         <script type="text/javascript">
-            var distancia=0;
-            function haversineDistance(lat1,lon1,lat2,lon2) 
-            {
-                function toRad(x) 
-                {
-                  return x * Math.PI / 180;
-                }
-
-                var R = 6371; // km
-
-                var x1 = lat2 - lat1;
-                var dLat = toRad(x1);
-                var x2 = lon2 - lon1;
-                var dLon = toRad(x2)
-                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                  Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-                  Math.sin(dLon / 2) * Math.sin(dLon / 2);
-                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-                var d = R * c;
-                
-                return d;
-              }
-            
-            var distanciaTotal = 0;
-            function  calculaDistancia(lastLat, lastLong, lat, long)
-            {
-                var distanciaEntreDoisPontos =  haversineDistance(lastLat, lastLong, lat, long);
-                distanciaTotal += distanciaEntreDoisPontos;
-                gravaDistancia(distanciaTotal);
-            }
-
-            function gravaDistancia(distanciaTotal)
-            {
-                $("#distanciaLabel").html( arredondarDistancia(distanciaTotal) + " Km" );
-            }
-            
-            function arredondarDistancia(distanciaTotal)
-            {
-                return distanciaTotal.toFixed(1);
-            }
-            
-            function finish()
-            {
-                var escolha;
-                escolha = window.confirm("Tem certeza que deseja temrinar a gravação ?");
-                if (escolha) {
-                    $("#distanciaHidden").value = parseFloat( arredondarDistancia(distanciaTotal) );
-                    $("#form").submit();
-                }
-            }
-
-            var pausado = false;
-            function alteraEstadoGravacao()
-            {
-                pausado = !pausado;
-                if (pausado) {
-                    document.getElementById("pause").innerHTML = "Play";
-                    turnOffGetLocation();
-                } else {
-                    document.getElementById("pause").innerHTML = "Pause";
-                    getLocation();
-                }
-            }
-
-            function showError(error)
+         function showError(error)
             {
                 switch (error.code)
                 {
@@ -163,6 +102,83 @@ and open the template in the editor.
                 navigator.geolocation.clearWatch(id);
             }
         </script>
+        
+        <!--Functions about distance:-->
+        <script type="text/javascript">
+        var distancia=0;
+            function haversineDistance(lat1,lon1,lat2,lon2) 
+            {
+                function toRad(x) 
+                {
+                  return x * Math.PI / 180;
+                }
+
+                var R = 6371; // km
+
+                var x1 = lat2 - lat1;
+                var dLat = toRad(x1);
+                var x2 = lon2 - lon1;
+                var dLon = toRad(x2)
+                var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                  Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+                  Math.sin(dLon / 2) * Math.sin(dLon / 2);
+                var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+                var d = R * c;
+                
+                return d;
+              }
+            
+            var distanciaTotal = 0;
+            function  calculaDistancia(lastLat, lastLong, lat, long)
+            {
+                var distanciaEntreDoisPontos =  haversineDistance(lastLat, lastLong, lat, long);
+                distanciaTotal += distanciaEntreDoisPontos;
+                gravaDistancia(distanciaTotal);
+            }
+
+            function gravaDistancia(distanciaTotal)
+            {
+                $("#distanciaLabel").html( arredondarDistancia(distanciaTotal) + " Km" );
+            }
+            
+            function arredondarDistancia(distanciaTotal)
+            {
+                return distanciaTotal.toFixed(1);
+            }
+        </script>
+        
+        <!--Functions about the timer-->
+        <script type="text/javascript">
+        
+        </script>
+        
+        <!--Another mixed functions:-->
+        <script type="text/javascript">
+            function finish()
+            {
+                var escolha;
+                escolha = window.confirm("Tem certeza que deseja temrinar a gravação ?");
+                if (escolha) {
+                    $("#distanciaHidden").value = parseFloat( arredondarDistancia(distanciaTotal) );
+                    $("#form").submit();
+                }
+            }
+
+            var pausado = false;
+            function alteraEstadoGravacao()
+            {
+                pausado = !pausado;
+                if (pausado) {
+                    document.getElementById("pause").innerHTML = "Play";
+                    turnOffGetLocation();
+                } else {
+                    document.getElementById("pause").innerHTML = "Pause";
+                    getLocation();
+                }
+            }
+        </script>
+        
+        <!--Functions to initialize the recording onload-->
         <script type="text/javascript">
             $(document).ready(function() {
                 gravaDistancia(0);
